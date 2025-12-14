fab062682ff60daff0ce5f000c09a7e7e6a29ebb
KGFCH2 <babinbid05@gmail.com>
2025-12-15 00:59:37 +0530

    App Logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,47 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { BookingProvider } from './context/BookingContext';
+import { ThemeProvider } from './context/ThemeContext';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 import HomePage from './pages/HomePage';
 import BookingPage from './pages/BookingPage';
+import ServicesPage from './pages/ServicesPage';
+import VehiclesPage from './pages/VehiclesPage';
+import PricingPage from './pages/PricingPage';
+import FAQPage from './pages/FAQPage';
+import BlogPage from './pages/BlogPage';
+import ContactPage from './pages/ContactPage';
+import AboutPage from './pages/AboutPage';
 
 function App() {
   return (
-    <BookingProvider>
-      <Router>
-        <div className="flex flex-col min-h-screen">
-          <Header />
-          <main className="flex-grow">
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/book-now" element={<BookingPage />} />
-              <Route path="*" element={<HomePage />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
-    </BookingProvider>
+    <ThemeProvider>
+      <BookingProvider>
+        <Router>
+          <ScrollToTop />
+          <div className="flex flex-col min-h-screen bg-white dark:bg-slate-950 transition-colors duration-300">
+            <Header />
+            <main className="flex-grow">
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/book-now" element={<BookingPage />} />
+                <Route path="/services" element={<ServicesPage />} />
+                <Route path="/vehicles" element={<VehiclesPage />} />
+                <Route path="/pricing" element={<PricingPage />} />
+                <Route path="/faq" element={<FAQPage />} />
+                <Route path="/blog" element={<BlogPage />} />
+                <Route path="/contact" element={<ContactPage />} />
+                <Route path="/about" element={<AboutPage />} />
+                <Route path="*" element={<HomePage />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </Router>
+      </BookingProvider>
+    </ThemeProvider>
   );
 }
 
